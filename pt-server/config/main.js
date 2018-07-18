40eea80ea774e3a51c120b4f3d616b07bd0f9801
vthomas <v.thomas@it-akademy.fr>
2018-07-18 07:20:37 +0000

    add routes

---
+++ pt-server/config/main.js
@@ -0,0 +1,8 @@
+module.exports = {
+  // Secret key for JWT signing and encryption
+  'secret': 'mysecretpassphrase',
+  // Database connection information
+  'database': 'mongodb://localhost:27017',
+  // Setting port for server
+  'port': 3000
+}
