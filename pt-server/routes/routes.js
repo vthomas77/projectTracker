8550e4886790b08a9f4539b89111abfb71ab6313
MourlonG <mourlonguillaume@gmail.fr>
2018-07-23 14:39:22 +0000

    CRUD taskgroup

--- pt-server/routes/routes.js
+++ pt-server/routes/routes.js
@@ -4,6 +4,7 @@ const passportLocalService = require('../config/passportLocal');
 const passportJWTService = require('../config/passportJWT');
 const AuthenticationController = require('../controllers/authentication');
 const ProjectController = require('../controllers/project');
+const TaskGroupController = require('../controllers/taskgroup');
 
 // Authenticate with JSON Web Token
 const requireAuth = passport.authenticate('jwt', { session: false });
@@ -17,6 +18,7 @@ module.exports = function(app) {
   const apiRoutes = express.Router();
   const authRoutes = express.Router();
   const projectRoutes = express.Router();
+  const taskgroupRoutes = express.Router();
 
   // Set api route group
   app.use('/api', apiRoutes);
@@ -53,5 +55,24 @@ module.exports = function(app) {
 
   // Delete project route
   projectRoutes.post('/delete', requireAuth, ProjectController.delete);
+
+  // ----------
+  // Task-Group
+  // ----------
+
+  // Set task group sub route group
+  apiRoutes.use('/task-group', taskgroupRoutes);
+
+  // Create task group route
+  taskgroupRoutes.post('/create', requireAuth, TaskGroupController.create);
+
+  // List task group route
+  taskgroupRoutes.post('/listAll', requireAuth, TaskGroupController.listAll);
+
+  // Update task group route
+  taskgroupRoutes.post('/update', requireAuth, TaskGroupController.update);
+
+  // Delete task group route
+  taskgroupRoutes.post('/delete', requireAuth, TaskGroupController.delete);
 
 };
