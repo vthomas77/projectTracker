87cf70fa84ed637a2db5b5539808bdeb3499782c
Kaskwoute <antoine.peyrin@hotmail.fr>
2018-07-24 09:56:56 +0200

    CRUD entity

--- pt-client/src/renderer/app/entity/controllers/EntityController.js
+++ pt-client/src/renderer/app/entity/controllers/EntityController.js
@@ -4,4 +4,10 @@ EntityController.$inject = ['RouteHelperService', '$location'];
 export default /*@ngInject*/ function EntityController( RouteHelperService, $location ) {
     var vm = this;
 
+    vm.entity = RouteHelperService.get();
+
+    // If id is 0, then it means it's a entity creation
+    vm.edit = vm.entity.entityId == 0 ? true : false;
+
+
 };
