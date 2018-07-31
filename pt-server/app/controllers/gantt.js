f6649fda43254ed73749070a9a79e1f12f0f4f37
vthomas <v.thomas@it-akademy.fr>
2018-07-31 13:48:28 +0000

    add project to gantt

--- pt-server/app/controllers/gantt.js
+++ pt-server/app/controllers/gantt.js
@@ -15,13 +15,19 @@ exports.show = function(req, res) {
       if (err) { return next(err); }
       Taskgroup.find({id_project: projectID}).sort('position').exec(function(err, taskgroup) {
       let dataArray = [];
+      dataArray.push({
+        id : 1,
+        text : project.name,
+        start_date : moment(project.starting_date).format("DD-MM-YYYY").toString(),
+        duration : 0,
+      });
       for (i=0;i<taskgroup.length;i++){
         dataArray.push({
-          id : i + 1,
+          id : i + 2,
           text : taskgroup[i].name_task_group,
           start_date : moment(taskgroup[i].starting_date).format("DD-MM-YYYY").toString(),
           duration : moment(taskgroup[i].end_date).diff(moment(taskgroup[i].starting_date),'days'),
-          parent : i
+          parent : 1
         });
       }
       return res.json({"data":dataArray});
