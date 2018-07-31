19b9056076e58d5a740626c916f10f838b85b3b2
Kaskwoute <antoine.peyrin@hotmail.fr>
2018-07-31 15:50:35 +0200

    FE : gantt crud

--- pt-client/src/renderer/app/gantt/controllers/GanttController.js
+++ pt-client/src/renderer/app/gantt/controllers/GanttController.js
@@ -4,15 +4,15 @@ export default /*@ngInject*/ function GanttController( $scope ) {
     var vm = this;
     $scope.tasks = {
         data:[
-            {id:1, text:"Project #2", start_date:"01-04-2018", duration:18,order:10, progress:0.4, open: true},
-            {id:2, text:"Task #1", start_date:"02-04-2018", duration:8, order:10, progress:0.6, parent:1},
-            {id:3, text:"Task #2", start_date:"11-04-2018", duration:8, order:20, progress:0.6, parent:1}
+            {id:1, text:"Project #1", start_date:"01-04-2018", duration:18,  open: true},
+            {id:2, text:"TaskGroup #1", start_date:"02-04-2018", duration:16, parent:1},
+            {id:3, text:"Task #1", start_date:"02-04-2018", duration:8, parent:2},
+            {id:4, text:"Task #2", start_date:"02-04-2018", duration:8, parent:2}
         ],
         links:[
-            { id:1, source:1, target:2, type:"1"},
-            { id:2, source:2, target:3, type:"0"},
-            { id:3, source:3, target:4, type:"0"},
-            { id:4, source:2, target:5, type:"2"}
+            // { id:1, source:1, target:2, type:"1"},
+            // { id:2, source:2, target:3, type:"0"},
+            // { id:4, source:1, target:3, type:"1"},
         ]
     };
 };
