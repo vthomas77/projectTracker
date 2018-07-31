b0b84e7c71c4ef2e3d0a7a2d66226bbe99caed21
vthomas <v.thomas@it-akademy.fr>
2018-07-31 15:06:14 +0000

    add task to gantt

--- pt-server/app/controllers/gantt.js
+++ pt-server/app/controllers/gantt.js
@@ -1,12 +1,15 @@
 const moment = require('moment');
 const Project = require('../models/projectModel');
 const Taskgroup = require('../models/taskgroupModel');
+const Task = require('../models/taskModel');
 
 // -------------
 // Show Gantt
 // -------------
-
+/*
 exports.show = function(req, res) {
+
+  let dataArray = [];
 
   const projectID = req.params.id;
 
@@ -14,23 +17,97 @@ exports.show = function(req, res) {
 
       if (err) { return next(err); }
       Taskgroup.find({id_project: projectID}).sort('position').exec(function(err, taskgroup) {
-      let dataArray = [];
+
+      k=1;
       dataArray.push({
-        id : 1,
+        id : k,
         text : project.name,
         start_date : moment(project.starting_date).format("DD-MM-YYYY").toString(),
         duration : 0,
       });
       for (i=0;i<taskgroup.length;i++){
+        k+=1;
+        tgIndex=k;
         dataArray.push({
-          id : i + 2,
+          id : k,
           text : taskgroup[i].name_task_group,
           start_date : moment(taskgroup[i].starting_date).format("DD-MM-YYYY").toString(),
           duration : moment(taskgroup[i].end_date).diff(moment(taskgroup[i].starting_date),'days'),
           parent : 1
         });
+        Task.find({id_task_group: taskgroup[i]._id}).sort('starting_date').exec(function(err, task) {
+            for (j=0;j<task.length;j++){
+              k+=1;
+              dataArray.push({
+                id : k,
+                text : task[j].name_task,
+                start_date : moment(task[j].starting_date).format("DD-MM-YYYY").toString(),
+                duration : moment(task[j].end_date).diff(moment(task[j].starting_date),'days'),
+                parent : tgIndex
+              });
+            }
+return res.json({"data":dataArray});
+          });
       }
-      return res.json({"data":dataArray});
+
     });
+
   });
+
+}
+*/
+exports.show = function(req, res) {
+
+  let dataArray = [];
+
+  const projectID = req.params.id;
+
+  Project.findById(projectID, function(err, project) {
+
+      if (err) { return next(err); }
+      Taskgroup.find({id_project: projectID}).sort('position').exec(function(err, taskgroup) {
+        if (err) { return next(err); }
+        const tgs = taskgroup.map(function (tg) { return tg; });
+        const taskgroupIDs = taskgroup.map(function (tg) { return tg._id; });
+        Task.find({id_task_group: {$in:taskgroupIDs}}).sort('starting_date').exec(function(err, task) {
+          const tsks = task.map(function (tsk) { return tsk; });
+          if (err) { return next(err); }
+          k=1;
+          dataArray.push({
+            id : k,
+            text : project.name,
+            start_date : moment(project.starting_date).format("DD-MM-YYYY").toString(),
+            duration : 0,
+          });
+          for (j=0;j<tgs.length;j++){
+            k+=1;
+            tgIndex=k;
+            dataArray.push({
+              id : k,
+              text : tgs[j].name_task_group,
+              start_date : moment(tgs[j].starting_date).format("DD-MM-YYYY").toString(),
+              duration : moment(tgs[j].end_date).diff(moment(tgs[j].starting_date),'days'),
+              parent : 1
+            });
+            for (i=0;i<tsks.length;i++){
+              if (tsks[i].id_task_group == tgs[j]._id)
+              {
+                k+=1;
+                dataArray.push({
+                  id : k,
+                  text : tsks[i].name_task,
+                  start_date : moment(tsks[i].starting_date).format("DD-MM-YYYY").toString(),
+                  duration : moment(tsks[i].end_date).diff(moment(task[i].starting_date),'days'),
+                  parent : tgIndex
+                });
+              }
+            }
+          }
+          return res.json({"data":dataArray});
+        });
+
+    });
+
+  });
+
 }
