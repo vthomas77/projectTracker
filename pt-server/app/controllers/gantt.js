04fd14eb6e6d6799acf07db1b743ddece4f403da
vthomas <v.thomas@it-akademy.fr>
2018-08-01 12:47:54 +0000

    add https

--- pt-server/app/controllers/gantt.js
+++ pt-server/app/controllers/gantt.js
@@ -6,56 +6,7 @@ const Task = require('../models/taskModel');
 // -------------
 // Show Gantt
 // -------------
-/*
-exports.show = function(req, res) {
 
-  let dataArray = [];
-
-  const projectID = req.params.id;
-
-  Project.findById(projectID, function(err, project) {
-
-      if (err) { return next(err); }
-      Taskgroup.find({id_project: projectID}).sort('position').exec(function(err, taskgroup) {
-
-      k=1;
-      dataArray.push({
-        id : k,
-        text : project.name,
-        start_date : moment(project.starting_date).format("DD-MM-YYYY").toString(),
-        duration : 0,
-      });
-      for (i=0;i<taskgroup.length;i++){
-        k+=1;
-        tgIndex=k;
-        dataArray.push({
-          id : k,
-          text : taskgroup[i].name_task_group,
-          start_date : moment(taskgroup[i].starting_date).format("DD-MM-YYYY").toString(),
-          duration : moment(taskgroup[i].end_date).diff(moment(taskgroup[i].starting_date),'days'),
-          parent : 1
-        });
-        Task.find({id_task_group: taskgroup[i]._id}).sort('starting_date').exec(function(err, task) {
-            for (j=0;j<task.length;j++){
-              k+=1;
-              dataArray.push({
-                id : k,
-                text : task[j].name_task,
-                start_date : moment(task[j].starting_date).format("DD-MM-YYYY").toString(),
-                duration : moment(task[j].end_date).diff(moment(task[j].starting_date),'days'),
-                parent : tgIndex
-              });
-            }
-return res.json({"data":dataArray});
-          });
-      }
-
-    });
-
-  });
-
-}
-*/
 exports.show = function(req, res) {
 
   let dataArray = [];
@@ -132,6 +83,7 @@ exports.show = function(req, res) {
             text : project.name,
             start_date : moment(project.starting_date).format("DD-MM-YYYY").toString(),
             duration : endProject.diff(moment(project.starting_date),'days') + 1,
+            open: true
           });
           return res.json({"data":dataArray,"links":linkArray});
         });
