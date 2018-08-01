0a05a95016fc53947fb7b9f98dd579bade628eb3
Kaskwoute <antoine.peyrin@hotmail.fr>
2018-08-01 14:27:52 +0200

    FE : gantt reload properly

--- pt-client/src/renderer/app/entity/resources/EntityResource.js
+++ pt-client/src/renderer/app/entity/resources/EntityResource.js
@@ -3,7 +3,7 @@
 EntityResource.$inject = ['$resource', '$http', 'clientConfig'];
 export default /*@ngInject*/ function EntityResource( $resource, $http, clientConfig ) {
     return {
-        Entity: $resource( 'http://' + clientConfig.API_URL + '/api/:entityType/:entityId/:action', { entityType: '@entityType' }, {
+        Entity: $resource( 'https://' + clientConfig.API_URL + '/api/:entityType/:entityId/:action', { entityType: '@entityType' }, {
             create: { params: { action: 'create' }, method: 'POST' },
             update : { params: { entityId: '@entityId' }, method: 'PUT' },
             get: { params: { entityId: '@entityId' }, method: 'GET' }
